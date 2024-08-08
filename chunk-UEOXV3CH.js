787e1b7145931d96aa6306e4a9242562e718a586
dcbroad3 <dcbroad3@users.noreply.github.com>
2024-08-08 15:14:26 +0000

    Deploying to main from @ factoriolab/factoriolab@2cc771fdebb4a84d1d0fcfc62d60eeca99797646 🚀

---
+++ chunk-UEOXV3CH.js
@@ -0,0 +1,1 @@
+import{a as n}from"./chunk-NEYKPSFL.js";import{la as r,qa as i}from"./chunk-S2PCDOQL.js";var u=(()=>{class e{displaySvc=r(n);transform(t){return this.displaySvc.toBonusPercent(t)}static \u0275fac=function(o){return new(o||e)};static \u0275pipe=i({name:"bonusPercent",type:e,pure:!0,standalone:!0})}return e})();export{u as a};
