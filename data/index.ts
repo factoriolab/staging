d530b4a3a72a2f23c1a29690cf8d8bbec161b5d0
dcbroad3 <dcbroad3@users.noreply.github.com>
2024-09-16 13:08:50 +0000

    Deploying to main from @ factoriolab/factoriolab@41b62eac4a06b8569b800e45f1ec65da0f424863 🚀

--- data/index.ts
+++ data/index.ts
@@ -49,7 +49,7 @@ export const data: AppData = {
     { id: 'ffy', name: 'Final Factory', game: Game.FinalFactory },
     { id: 'ulc', name: 'Ultracube', game: Game.Factorio },
   ],
-  modHashV0: [
+  v0: [
     '1.1',
     '1.0',
     '0.17',
@@ -68,7 +68,7 @@ export const data: AppData = {
     'space-exploration',
     'xander',
   ],
-  modHash: [
+  hash: [
     '1.1',
     '1.0',
     '017',
