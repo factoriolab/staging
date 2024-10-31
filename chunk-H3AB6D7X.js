c62589685ff3a51e2b6237d3642eff770f73610b
dcbroad3 <dcbroad3@users.noreply.github.com>
2024-10-31 15:17:26 +0000

    Deploying to main from @ factoriolab/factoriolab@1b021cc947b0b61892749e045d1f51ca21c43b56 🚀

--- chunk-H3AB6D7X.js
+++
@@ -1,1 +0,0 @@
-import{c as n}from"./chunk-FADC2OY2.js";import{S as r,W as i}from"./chunk-TPZ4ZB6B.js";var u=(()=>{class t{constructor(){this.displaySvc=r(n)}transform(e){return this.displaySvc.toBonusPercent(e)}static{this.\u0275fac=function(s){return new(s||t)}}static{this.\u0275pipe=i({name:"bonusPercent",type:t,pure:!0,standalone:!0})}}return t})();export{u as a};
