767ea391bde652774497116908535a8753f69765
dcbroad3 <dcbroad3@users.noreply.github.com>
2026-05-31 02:14:47 +0000

    Deploying to main from @ factoriolab/factoriolab@b254aa4012d9e588b2446eb8e1197ad9a6d3a033 🚀

---
+++ chunk-V7UHNKTP.js
@@ -0,0 +1,1 @@
+import"./chunk-JGDRNPQP.js";var o=[{path:"list",loadComponent:()=>import("./chunk-H3WIUBO6.js").then(t=>t.Steps)},{path:"flow",loadComponent:()=>import("./chunk-TALVTUNT.js").then(t=>t.Flow)},{path:"data",loadChildren:()=>import("./chunk-NWZZRF4C.js").then(t=>t.routes)}];export{o as routes};
